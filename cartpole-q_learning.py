06ac1f105226c44586e6fbbd98ec5d88b21d2563
deepakkavoor <31254259+deepakkavoor@users.noreply.github.com>
2018-07-09 18:34:08 +0530

    Update cartpole-q_learning.py

--- cartpole-q_learning.py
+++ cartpole-q_learning.py
@@ -12,7 +12,7 @@ class CartPole():
 	def __init__(self, buckets = (1, 1, 6, 3,), n_episodes = 1000, solved_t = 195,
 				min_epsilon = 0.1, min_alpha = 0.1, gamma = 0.99):
 		self.buckets = buckets  #discrete values for each feature space dimension
-								#(position, velocity, angle, angular velocity)
+					#(position, velocity, angle, angular velocity)
 		self.n_episodes = n_episodes  #training episodes
 		self.min_alpha = min_alpha
 		self.min_epsilon = min_epsilon
@@ -63,8 +63,7 @@ class CartPole():
 	def update_table(self, old_state, action, reward, new_state, alpha):
 		#updates the state-action pairs based on future reward
 		new_state_Q_value = np.max(self.Q_table[new_state])
-		self.Q_table[old_state][action] += alpha * (reward + self.gamma * new_state_Q_value
-													- self.Q_table[old_state][action])
+		self.Q_table[old_state][action] += alpha * (reward + self.gamma * new_state_Q_value - self.Q_table[old_state][action])
 
 
 	def run(self):
